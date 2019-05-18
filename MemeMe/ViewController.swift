f16034578fcfc7964420e7df5d84d114834d1aa2
Glenn Cole <gcole@mac.com>
2019-05-18 14:41:39 -0700

    Camera/PhotoLibrary pickers functional AutoLayout constraint on image view still wrong (see landscape)

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MemeMe
+//  ImagePickerTabBar
 //
 //  Created by Glenn Cole on 5/18/19.
 //  Copyright © 2019 Glenn Cole. All rights reserved.
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum ToolbarTags : Int {
+    case camera = 0, album
+}
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        
+        let pc = UIImagePickerController()
+        
+        pc.sourceType = (sender.tag == ToolbarTags.camera.rawValue) ? .camera : .photoLibrary
+        pc.delegate = self
+        
+        present(pc, animated: true, completion: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
