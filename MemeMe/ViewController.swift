ce4f49146867b0b2d90503f237a9a8faf63838de
Glenn Cole <gcole@mac.com>
2019-05-19 01:09:01 -0700

    Mostly seems to work...but landscape still has issues with autolayout Super-painful to discover text fill uses NEGATIVE stroke width

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -12,21 +12,70 @@ enum ToolbarTags : Int {
     case camera = 0, album
 }
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    var activeTextField: UITextField?
+    let defaultTextTop = "Top"
+    let defaultTextBottom = "Bottom"
+    
+    
+    // MARK: - view load/appear/etc.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.strokeWidth: -3.0,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name:"HelveticaNeue-CondensedBlack", size:40)!,
+        ]
+        
+        setTextFieldDefaults(textField: topTextField, defaultText: defaultTextTop, attributes: memeTextAttributes)
+        setTextFieldDefaults(textField: bottomTextField, defaultText: defaultTextBottom, attributes: memeTextAttributes)
+    }
+    
+    func setTextFieldDefaults( textField: UITextField,
+                               defaultText: String,
+                               attributes: [NSAttributedString.Key: Any] ) {
+        
+        textField.defaultTextAttributes = attributes
+        textField.typingAttributes = attributes
+        textField.textAlignment = .center
+        textField.text = defaultText
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        updateMemeLabelsHidden()
+        subscribeToKeyboardNotifications()
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func updateMemeLabelsHidden() {
+        
+        if imageView.image == nil {
+            topTextField.isHidden = true
+            bottomTextField.isHidden = true
+        }
+        else {
+            topTextField.isHidden = false
+            bottomTextField.isHidden = false
+        }
     }
     
     @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
@@ -39,10 +88,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(pc, animated: true, completion: nil)
     }
     
+    
+    // MARK: - ImagePickerControllerDelegate methods
+    
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let selectedImage = info[.originalImage] as? UIImage {
             imageView.image = selectedImage
+            updateMemeLabelsHidden()
         }
         dismiss(animated: true, completion: nil)
     }
@@ -50,6 +103,102 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - TextFieldDelegate methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        activeTextField = textField
+        
+        if textField == topTextField {
+            if topTextField?.text == defaultTextTop {
+                topTextField?.text = ""
+            }
+        }
+        else if textField == bottomTextField {
+            if bottomTextField?.text == defaultTextBottom {
+                bottomTextField?.text = ""
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        activeTextField?.text = activeTextField?.text?.uppercased()
+        activeTextField = nil
+    }
+    
+    
+    // MARK: - Keyboard machinations
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShowOrHide(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShowOrHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+    }
+    
+//    @objc func adjustForKeyboard( _ notification: Notification ) {
+//        
+//        let userInfo = notification.userInfo
+//        let keyboardSize = userInfo![ UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+//
+//        let keyboardScreenEndFrame = keyboardSize.cgRectValue
+//        let keyboardViewEndFrame = view.convert( keyboardScreenEndFrame, from: view.window )
+//        
+//        if notification.name == UIResponder.keyboardWillHideNotification {
+//            
+//        }
+//    }
+
+    @objc func keyboardWillShowOrHide( _ notification: Notification ) {
+
+        guard let activeTextField = activeTextField else {
+            print( "No active text field?" )
+            return
+        }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification {
+//            view.frame.origin.y = -getKeyboardHeight( notification )
+            let keyboardRect = getKeyboardRect( notification )
+            let delta = activeTextField.frame.origin.y + activeTextField.frame.height + 10.0 - keyboardRect.origin.y
+            view.frame.origin.y = (delta > 0.0) ? 0.0 - delta : 0.0
+        }
+        else {
+            view.frame.origin.y = 0.0
+        }
+    }
+    
+    func getKeyboardRect(_ notification: Notification) -> CGRect {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![ UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue
     }
 }
 
